b17c91a3c301048c7e0d3c057d2c574c2e6e6798
Wasswa Enock Male <enockmale97@gmail.com>
2023-04-05 12:20:37 +0300

    I have merged and overrided the settings in prettier

--- TodoApp/.eslintrc.js
+++ TodoApp/.eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
+  // eslint-disable-next-line prettier/prettier
   extends: '@react-native-community',
+  extends: ["plugin:prettier/recommended"],
+  "prettier/prettier": [
+    "error",
+    {
+      "singleQuote": true,
+      "parser": "flow"
+    }
+  ]
 };
