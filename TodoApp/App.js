a0a345ba5a94f08ae456f2324d9728c64498c4c9
Wasswa Enock Male <enockmale97@gmail.com>
2023-04-06 16:51:06 +0300

    installed expo-splash-screen and expo-font

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,20 +1,20 @@
-/* eslint-disable prettier/prettier */
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-import {SplashScreen, Login} from './Components/'
-import React, { useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-function App() {
-  // When the application has loaded using the useEffect hook, then load the login page.
-  useEffect(()=>{
-    <Login />
-  }, []);
-
-  return <SplashScreen />;
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
