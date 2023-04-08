07084dd69bbf46feb4b2c1696097089634578ee1
Wasswa Enock Male <enockmale97@gmail.com>
2023-04-09 02:16:12 +0300

    created a new todoApp using expo

---
+++ todoApp/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{color: '#fff'}}>This actually works</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
