8a551442d160dd1c0cbb3e6755c5c39a2f81b135
Wasswa Enock Male <enockmale97@gmail.com>
2023-04-09 09:57:37 +0300

    footer for login

---
+++ todoApp/Components/LoginFooter.js
@@ -0,0 +1,29 @@
+import { StyleSheet,View, Text, Pressable } from "react-native"
+
+const LoginFooter = ()=>{
+    return(
+        <View>
+            <Text style={{textAlign:'center',fontSize:12,color:'#fff'}}>If you don't have an account!</Text>
+            <Pressable style={styles.press} onPress={() => alert("Sign up button clicked")}>
+                <Text style={styles.tpress}>Sign Up</Text>
+            </Pressable>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    press:{
+        backgroundColor:'#25292e',
+        padding:2,
+        margin: 5,
+        borderWidth:1,
+        borderColor:'#fff',
+        width:'29%',
+        alignSelf:'center'
+    },
+    tpress:{
+        color:'#fff',
+        textAlign:'center',
+    }
+})
+export default LoginFooter;
