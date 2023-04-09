dd37067630bb4fc5a319f572dc2cf1080e568ff2
Wasswa Enock Male <enockmale97@gmail.com>
2023-04-09 08:39:32 +0300

    Created LoginCard component

---
+++ todoApp/Components/LoginCard.js
@@ -0,0 +1,46 @@
+import {View, TextInput, StyleSheet, Button, Text} from 'react-native';
+import LoginFooter from './LoginFooter';
+
+const LoginCard = ()=>{
+    return(
+        <View style={styles.container}>
+            <Text style={styles.logText}>Login</Text>
+            <TextInput style={styles.textInput} placeholder="Enter email" placeholderTextColor='#fff'/>
+            <TextInput style={styles.textInput} placeholder="Enter password" placeholderTextColor='#fff'/>
+            <Button title="Login" style={styles.btn}/>
+            <LoginFooter/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        height:'60%',
+        width: '70%',
+        alignContent:'center',
+        justifyContent:'center',
+        // borderWidth:1,
+        padding:10,
+        margin:5,
+        // backgroundColor:'#fff'
+
+    },
+    textInput:{
+        borderRadius:5,
+        borderBottomWidth:1,
+        borderBottomColor:'#fff',
+        padding: 10,
+        margin:10,
+        color:'#fff',
+    },
+    logText:{
+        color:'#fff',
+        fontSize:30,
+        textAlign:'center'
+    },
+    btn:{
+        borderRadius:40
+    }
+});
+
+export default LoginCard;
