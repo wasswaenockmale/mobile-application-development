e286bd4c76d1ca69387a8e32fe88929c5aef7841
Wasswa Enock Male <enockmale97@gmail.com>
2023-04-11 17:11:12 +0300

    NavigationContainer and Stack to the application

--- todoApp/App.js
+++ todoApp/App.js
@@ -3,8 +3,12 @@
 // import DashBoard from './Components/DashBoard';
 import {useState, useEffect} from 'react';
 import SplashScreen from './Components/SplashScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './Components/Login';
+import SignUp from './Components/SignUp';
 
+const Stack = createNativeStackNavigator();
 
 export default function App(){
   const [splash, setSplash] = useState(false);
@@ -12,11 +16,20 @@ export default function App(){
   useEffect(()=>{
     setTimeout(()=>{
       setSplash(true);
-    },2500);
+    },2000);
   },[]); // Onmount
 
+
   if(splash){
-    return <Login />
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login}/>
+          <Stack.Screen name="SignUp" component={SignUp}/>
+          <Stack.Screen name='Home' component={'DashBoard'}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
   }else{
     return <SplashScreen />
   }
