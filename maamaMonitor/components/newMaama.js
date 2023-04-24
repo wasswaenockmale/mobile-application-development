1386b64f12e9124048a43f528acd77eb2c113561
Wasswa Enock Male <enockmale97@gmail.com>
2023-04-25 00:21:35 +0300

    created new interface for new mama captured info

--- maamaMonitor/components/newMaama.js
+++ maamaMonitor/components/newMaama.js
@@ -1,34 +1,92 @@
-import { TextInput, View, StyleSheet, Button } from "react-native"
+import {
+    View,
+    StyleSheet,
+    Text,
+    Dimensions,
+    TouchableOpacity
+} from 'react-native';
 
-const MaamaInfor = ()=>{
+
+import Background from '../helpers/background';
+import Field from '../helpers/Field'
+import Btn from '../helpers/Btn';
+
+const windowWidth = Dimensions.get('screen').width;
+const windowHeight = Dimensions.get('window').height;
+
+const Sign = (props) => {
+
     return(
-        <View style={styles.container}>
-            <TextInput style={styles.textInput} passwordRules={true} placeholder="Your name"/>
-            <TextInput style={styles.textInput} placeholder="Age"/>
-            <TextInput style={styles.textInput} placeholder="LNMP"/>
-            <TextInput style={styles.textInput} placeholder="Next of kin"/>
-            <Button title="Submit" color={'green'} style={{width: '50%'}}/>
-        </View>
+        <Background>
+            <View style={styles.container}>
+                <Text style={styles.text}>About you</Text>
+                <Text>We want to know more about you.</Text>
+                <View style={styles.innerView}>
+                    <Field placeholder="Your Full name"/>
+                    <Field placeholder="Age"/>
+                    <Field placeholder="LNMP"/>
+                    <Field placeholder="Next of Kin"/>
+                    <Field placeholder="NOK's Phone number"/>
+                    <Btn Width="90%" textColor="white" bgColor="#4b6043" btnLabel="Submit"/>
+                </View>
+            </View>
+        </Background>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        padding:40,
-        width:"100%",
-        height:'100%',
+        alignItems:'center'
     },
+    innerView:{
+        backgroundColor:'white',
+        height:windowHeight,
+        width:windowWidth,
+        paddingTop:50,
+        borderTopLeftRadius:90,
+        borderTopRightRadius:90,
+        alignItems:'center',
+    },
+    text:{
+        fontSize:50,
+        color:'white'
+    },
+})
 
-    textInput:{
-        width:"90%",
-        height:40,
-        borderColor:'green',
-        borderWidth:1,
-        borderRadius: 5,
-        margin:5,
-        fontSize:15,
 
-    }
-});
+export default Sign;
 
-export default MaamaInfor;
+// import { TextInput, View, StyleSheet, Button } from "react-native"
+
+// const MaamaInfor = ()=>{
+//     return(
+//         <View style={styles.container}>
+//             <TextInput style={styles.textInput} passwordRules={true} placeholder="Your name"/>
+//             <TextInput style={styles.textInput} placeholder="Age"/>
+//             <TextInput style={styles.textInput} placeholder="LNMP"/>
+//             <TextInput style={styles.textInput} placeholder="Next of kin"/>
+//             <Button title="Submit" color={'green'} style={{width: '50%'}}/>
+//         </View>
+//     )
+// }
+
+// const styles = StyleSheet.create({
+//     container:{
+//         padding:40,
+//         width:"100%",
+//         height:'100%',
+//     },
+
+//     textInput:{
+//         width:"90%",
+//         height:40,
+//         borderColor:'green',
+//         borderWidth:1,
+//         borderRadius: 5,
+//         margin:5,
+//         fontSize:15,
+
+//     }
+// });
+
+// export default MaamaInfor;
