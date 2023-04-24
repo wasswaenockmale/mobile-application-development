0ee8b37d59dd148b37b1bf073fe10272a34a6239
Wasswa Enock Male <enockmale97@gmail.com>
2023-04-24 16:15:42 +0300

    Button component

---
+++ maamaMonitor/components/helpers/Btn.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+
+const Btn = props =>{
+    return(
+        <TouchableOpacity
+            onPress={props.Press}
+            style={{
+                backgroundColor: props.bgColor,
+                borderRadius:100,
+                alignItems: 'center',
+                width: props.Width,
+                paddingVertical: 5,
+                marginBottom:20,
+            }}>
+                <Text style={{
+                    color: props.textColor,
+                    fontSize:25,
+                    fontWeight: 'bold'
+                }}>{props.btnLabel}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export default Btn;
