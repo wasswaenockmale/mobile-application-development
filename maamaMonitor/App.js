3f05e2dbc3e6f8d537add6b62b80877d6d73de9f
Wasswa Enock Male <enockmale97@gmail.com>
2023-04-24 16:10:12 +0300

    Added the navigation screen to App.js file

--- maamaMonitor/App.js
+++ maamaMonitor/App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './components/Screens/Home';
+import Login from './components/Screens/Login';
+import Sign from './components/Screens/Sign';
+import History from './components/Screens/History'
+import Welcome from './components/Screens/WelcomeScreen';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Maama Monitor</Text>
-      <StatusBar style="inverted"/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name='Welcome' component={Welcome}/>
+        <Stack.Screen name='Home' component={Home}/>
+        <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='Sign' component={Sign} />
+        <Stack.Screen name='History' component={History} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
