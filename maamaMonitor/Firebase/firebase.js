641017df1a7c6ca3ce3b412c82d4039daf030063
Wasswa Enock Male <enockmale97@gmail.com>
2023-04-28 16:32:18 +0300

    Changed the firebase name from config to firebase

--- maamaMonitor/Firebase/firebase.js
+++ maamaMonitor/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +16,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
