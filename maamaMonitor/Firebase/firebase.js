37514fb44266a125e2a90fc6ca0cdd00b540c70f
Wasswa Enock Male <enockmale97@gmail.com>
2023-04-28 17:09:53 +0300

    got real time database setup

--- maamaMonitor/Firebase/firebase.js
+++ maamaMonitor/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +20,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth();
+export const database = getDatabase();
