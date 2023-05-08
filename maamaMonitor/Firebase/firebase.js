a62712a6c541be133f68f945bca90cac496ff7d4
wasswaenockmale <enockmale97@gmail.com>
2023-05-08 22:10:29 +0300

    created the file for Doctor

--- maamaMonitor/Firebase/firebase.js
+++ maamaMonitor/Firebase/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
+// import { getAuth } from 'firebase/auth';
+// import { getDatabase } from 'firebase/database';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +19,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth();
-export const database = getDatabase(app);
+// export const auth = getAuth();
+// export const database = getDatabase(app);
---
+++ maamaMonitor/components/Screens/Doctor.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, View } from "react-native"
+
+const Doctor = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                The doctor should see a list of patients.git
+            </View>
+        </SafeAreaView>
+    )
+}
