3125ede22ecfb9dc7cc23b74181c2f367a3e77d8
wasswaenockmale <enockmale97@gmail.com>
2023-05-10 15:39:53 +0300

    roofing the welcome screen

--- maamaMonitor/components/Screens/WelcomeScreen.js
+++ maamaMonitor/components/Screens/WelcomeScreen.js
@@ -5,14 +5,14 @@ import Btn from "../helpers/Btn";
 
 const WelcomeScreen = (props) =>{
     return(
-        <Background>
+        <>
             <View style={styles.container}>
-                <Text style={{...styles.text, marginTop:50}}>Welcome to</Text>
-                <Text style={{...styles.text, marginBottom: 60}}>Maama Monitor</Text>
-                <Btn Width="100%" bgColor="green" textColor="white" btnLabel="Login" Press={() =>props.navigation.navigate('Login')}/>
-                <Btn Width="100%" bgColor="white" textColor="green" btnLabel="Sign up" Press={() => props.navigation.navigate('Sign')}/>
+                <Text style={{...styles.text, marginTop:50}}>Maama Monitor</Text>
+                <Text style={{...styles.text, marginBottom: 60, fontSize:20}}>Welcomes you</Text>
+                <Btn Width="100%" bgColor="purple" textColor="white" btnLabel="Login" borderWidth={2} borderColor="purple" Press={() =>props.navigation.navigate('Login')}/>
+                <Btn Width="100%" bgColor="white" textColor="purple" btnLabel="Sign up" borderWidth={2} borderColor="purple" Press={() => props.navigation.navigate('Sign')}/>
             </View>
-        </Background>
+        </>
     )
 }
 
@@ -20,11 +20,13 @@ const styles = StyleSheet.create({
     container:{
         marginHorizontal: 40,
         marginVertical:100,
+        // backgroundColor:'purple'
         // borderWidth:1
     },
     text:{
-        color:'white',
+        color:'purple',
         fontSize:40,
+        textAlign: "center"
     }
 })
 export default WelcomeScreen;
