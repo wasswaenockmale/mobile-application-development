02b8491930dc4c06e65a4f3bb13c0d8515c408be
wasswaenockmale <enockmale97@gmail.com>
2023-05-12 09:25:08 +0300

    cleaning up history

--- maamaMonitor/components/Screens/History.js
+++ maamaMonitor/components/Screens/History.js
@@ -1,7 +1,7 @@
 import React from "react";
-import {View, Text,StyleSheet} from 'react-native';
-import Background from "../helpers/background";
+import {View, Text,StyleSheet, StatusBar} from 'react-native';
 import { Table, TableWrapper, Row,Rows, Col, Cols, Cell } from "react-native-table-component";
+import { AntDesign } from "@expo/vector-icons";
 
 const History = (props) => {
 
@@ -16,29 +16,58 @@ const History = (props) => {
         ]
     }
     return(
-       <Background>
+       <>
              <View style={styles.contain}>
-                <Text style={{color:'white'}}>No records yet, new app</Text>
-                <Table>
-                    <Row data={tableData.tableHead} style={styles.head} textStyle={styles.text} />
-                    <Rows data={tableData.tabledb} style={styles.text} textStyle={styles.text}/>
-                </Table>
+                <View style={styles.header}>
+                        <AntDesign name="left" size={24} color="white" style={{}} onPress={() => {
+                            props.navigation.navigate('Home');
+                        }}/>
+                    <View style={styles.left}>
+                        <Text style={styles.text_heading}>Your medical history</Text>
+                    </View>
+                </View>
+                <View>
+                    <Text style={{color:'purple'}}>No records yet, new app</Text>
+                    <Table>
+                        <Row data={tableData.tableHead} style={styles.head} textStyle={styles.text} />
+                        <Rows data={tableData.tabledb} style={styles.text} textStyle={styles.text}/>
+                    </Table>
+                </View>
+                <StatusBar style="auto" />
             </View>
-       </Background>
+       </>
     )
 }
 
 const styles = StyleSheet.create({
     contain:{
-        padding:40,
+        padding:0,
+        height:'100%',
+        width: '100%',
+    },
+    header:{
+        backgroundColor:'purple',
+        width:'100%',
+        height:'10%',
+        flexDirection:"row",
+        padding:20
+    },
+    left:{
+        width:'90%',
+        alignSelf:'stretch',
     },
     head:{
-        color:'white',
+        color:'purple',
         fontSize: 30
     },
     text:{
-        color:'white',
+        color:'purple',
         fontSize: 15
+    },
+    text_heading:{
+        color:'white',
+        fontSize: 15,
+        textAlign:'center'
     }
 })
 
